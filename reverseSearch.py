d99a620253481ae9a5c0248fa2ba89eaaf039bf6
Qi Zhang <zhang_qi@wheatoncollege.edu>
2016-01-23 13:21:58 -0500

    Add template and .gitignore

--- reverseSearch.py
+++ reverseSearch.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html', display=True)
+    return render_template('reverseSearch.html')
 
 app.debug = True
 if __name__ == "__main__":

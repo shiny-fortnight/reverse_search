018096aa96fbc34b4bf1fe940b048e4afe63af2f
Qi Zhang <zhang_qi@wheatoncollege.edu>
2016-01-23 15:46:26 -0500

    Include Whitepages APIs

--- reverseSearch.py
+++ reverseSearch.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, request
 from FacebookResetPasswordAPI import FacebookResetPasswordAPI
+from whitepages_api import makeAPIRequest
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
     if request.method == "GET":
-        return render_template('reverseSearch.html', new=True)
+        return render_template('reverseSearch.html', resultFound=False)
     if request.method == "POST":
-        emailQuery = request.form['email']
-        res = FacebookResetPasswordAPI({'verbose': True}).get(emailQuery)
-        profilePicture = res['profile_picture'][:-2]+'250'
-        profileName = res['full_name']
-        return render_template('reverseSearch.html', new=False, res=res, profilePicture=profilePicture, profileName=profileName)
+        query = request.form['email']
+        # facebookRes = FacebookResetPasswordAPI({'verbose': True}).get(query)
+        # profilePicture = facebookRes['profile_picture'][:-2]+'250'
+        # profileName = facebookRes['full_name']
+
+        try:
+            whitepateRes = makeAPIRequest(str(query))
+        except Exception, e:
+            whitepateRes = e
+        print whitepateRes
+        return render_template('reverseSearch.html', resultFound=True, whitepateRes=whitepateRes)
+        # return render_template('reverseSearch.html', resultFound=True, profilePicture=profilePicture, profileName=profileName, whitepateRes=whitepateRes)
 
 app.debug = True
 if __name__ == "__main__":

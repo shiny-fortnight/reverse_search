44ef3422190823c663bd18bea782f9f7ee7582f6
Qi Zhang <zhang_qi@wheatoncollege.edu>
2016-01-23 14:55:34 -0500

    Hook up to the Facebook Reset Password API

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# reverse_search
+# Reverse Search
+###Setup
+``` shell
+$ pip install flask
+python reverseSearch.py
+```
+###Browser 
+`http://localhost:5000`
+
+###Resource
+[PaulSec/osint-facebook-reset-password](https://github.com/PaulSec/osint-facebook-reset-password`)
--- reverseSearch.py
+++ reverseSearch.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from FacebookResetPasswordAPI import FacebookResetPasswordAPI
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello():
-    return render_template('reverseSearch.html')
+    if request.method == "GET":
+        return render_template('reverseSearch.html', new=True)
+    if request.method == "POST":
+        emailQuery = request.form['email']
+        res = FacebookResetPasswordAPI({'verbose': True}).get(emailQuery)
+        profilePicture = res['profile_picture'][:-2]+'250'
+        profileName = res['full_name']
+        return render_template('reverseSearch.html', new=False, res=res, profilePicture=profilePicture, profileName=profileName)
 
 app.debug = True
 if __name__ == "__main__":

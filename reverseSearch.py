f8ee09a8c1e7bd37c48c9a42f12afcf65d9c043d
Qi Zhang <zhang_qi@wheatoncollege.edu>
2016-01-24 00:03:43 -0500

    Bind port and set host

--- reverseSearch.py
+++ reverseSearch.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from FacebookResetPasswordAPI import FacebookResetPasswordAPI
 from whitepages_api import makeAPIRequest
@@ -42,4 +43,5 @@ def hello():
 
 app.debug = True
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

79be8d2d4a07b09fe7f68acf75804db6fbe9fa16
Shiwei Huang <80:3f:9a:17:05:f8:9c:87:45:e7:c4:d6:ed:50:f3:8d>
2016-01-24 13:49:31 -0500

    static changes

--- reverseSearch.py
+++ reverseSearch.py
@@ -1,50 +1,49 @@
-import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from FacebookResetPasswordAPI import FacebookResetPasswordAPI
 from whitepages_api import makeAPIRequest
-from OpenCNAMAPI import OpenCNAMAPI
-from calleridservice import call_calleridservice
-from GoogleReverseImageSearch import searchImage
-from geo_twitter_lookup import search_twitter
 app = Flask(__name__)
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
+    print "outside"
+    if request.method == "GET":
+        print "hahahhaa"
+        return render_template('index.html')
+
+    if request.method == "POST":
+        print "========"
+        print url_for('search')
+        return redirect(url_for('search'))
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+
+
     if request.method == "GET":
         return render_template('reverseSearch.html', resultFound=False)
 
     if request.method == "POST":
         query = request.form['email']
-        keywords = request.form['keywords']
         facebookRes = FacebookResetPasswordAPI({'verbose': True}).get(query)
         if facebookRes:
             profilePicture = facebookRes['profile_picture'][:-2]+'250'
             profileName = facebookRes['full_name']
-
-            googleReverseImageSearchRes = searchImage(profilePicture)
         else:
             profilePicture = None
             profileName = None
-            googleReverseImageSearchRes = None
 
         try:
             whitepateRes = makeAPIRequest(str(query))
         except Exception, e:
             whitepateRes = e
 
-        twitterRes = search_twitter(str(query), str(keywords))
+        # twitterRes = 
 
-        try:
-            openCNAMAPIRes = OpenCNAMAPI({'verbose': True}).get(query)
-        except Exception, e:
-            openCNAMAPIRes = e
-
-        calleridserviceRes = call_calleridservice(query)
-
-        return render_template('reverseSearch.html', resultFound=True, profilePicture=profilePicture, profileName=profileName, whitepateRes=whitepateRes, openCNAMAPIRes=openCNAMAPIRes, calleridserviceRes=calleridserviceRes, googleReverseImageSearchRes=googleReverseImageSearchRes, twitterRes=twitterRes)
+        return render_template('/reverseSearch.html', resultFound=True, profilePicture=profilePicture, profileName=profileName, whitepateRes=whitepateRes)
+        # return render_template('reverseSearch.html', resultFound=True, profilePicture=profilePicture, profileName=profileName, whitepateRes=whitepateRes, twitterRes=twitterRes)
 
 app.debug = True
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
